e4dd97ca7dcc76a369f657566e2e92285bf315d7
Mateusz Dymczyk <dymczyk@gmail.com>
2012-06-10 19:57:58 +0200

    chapter 10 exercise 2

---
+++ Chapter102.scala
@@ -0,0 +1,17 @@
+// Exercise 2
+import java.awt.geom.Ellipse2D
+import java.awt.Point 
+
+class OrderedPoint extends Point with scala.math.Ordered[Point] {
+   def compare(that : Point) = {
+      if( this.equals(that) ) 0
+      else if ((x > that.getX) || (x == that.getX && y > that.getY)) 1
+      else -1
+   }
+}
+
+object OrderedPoint {
+
+   def main(args : Array[String]) {
+   }
+}
