c632855035d464f9de1a839dd57d11e5a046b4d9
Mateusz Dymczyk <dymczyk@gmail.com>
2012-06-11 00:19:27 +0200

    chapter 10 exercise 4. not fully finished yet

--- chapter10/Chapter104.scala
+++ chapter10/Chapter104.scala
@@ -1,12 +1,20 @@
-// Exercise 2
-import java.awt.geom.Ellipse2D
-import java.awt.Point 
+// Exercise 4
 
-class OrderedPoint extends Point with scala.math.Ordered[Point] {
-   def compare(that : Point) = {
-      if( this.equals(that) ) 0
-      else if ((x > that.getX) || (x == that.getX && y > that.getY)) 1
-      else -1
+trait Logger {
+   def log(msg : String)
+}
+
+class CryptoLogger extends Logger  {
+   val key = 3
+
+   def log(msg : String) {
+      println(msg.map(c => caesar(c)))
+   }
+
+   private def caesar(c : Char) = c match {
+      case c if c isLower => ('a'+((c-'a'+key)%26)).toChar
+      case c if c isUpper => ('A'+((c-'A'+key)%26)).toChar
+      case _ => c
    }
 }
 
